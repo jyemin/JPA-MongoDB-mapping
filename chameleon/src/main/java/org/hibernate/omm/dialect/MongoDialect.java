964d00a33c24430d032c6f6978287948735bfbd0
nathan.xu <nathan.xu@procor.com>
2024-08-03 18:07:23 -0400

    add 'array_includes' function to MongoDialect

--- chameleon/src/main/java/org/hibernate/omm/dialect/MongoDialect.java
+++ chameleon/src/main/java/org/hibernate/omm/dialect/MongoDialect.java
@@ -1,17 +1,28 @@
 package org.hibernate.omm.dialect;
 
+import org.hibernate.boot.model.FunctionContributions;
 import org.hibernate.boot.model.TypeContributions;
 import org.hibernate.dialect.DatabaseVersion;
 import org.hibernate.dialect.Dialect;
+import org.hibernate.dialect.function.array.AbstractArrayContainsFunction;
 import org.hibernate.engine.jdbc.dialect.spi.DialectResolutionInfo;
+import org.hibernate.metamodel.mapping.JdbcMapping;
+import org.hibernate.metamodel.mapping.JdbcMappingContainer;
 import org.hibernate.omm.ast.MongoSqlAstTranslatorFactory;
 import org.hibernate.omm.type.ObjectIdJavaType;
 import org.hibernate.omm.type.ObjectIdJdbcType;
 import org.hibernate.omm.util.StringUtil;
+import org.hibernate.query.ReturnableType;
 import org.hibernate.service.ServiceRegistry;
+import org.hibernate.sql.ast.SqlAstTranslator;
 import org.hibernate.sql.ast.SqlAstTranslatorFactory;
 import org.hibernate.sql.ast.spi.SqlAppender;
+import org.hibernate.sql.ast.tree.SqlAstNode;
+import org.hibernate.sql.ast.tree.expression.Expression;
+import org.hibernate.type.BasicPluralType;
 import org.hibernate.type.spi.TypeConfiguration;
+
+import java.util.List;
 
 /**
  * @author Nathan Xu
@@ -63,4 +74,45 @@ public class MongoDialect extends Dialect {
         typeConfiguration.getJdbcTypeRegistry().addDescriptor(ObjectIdJdbcType.INSTANCE);
     }
 
+    @Override
+    public void initializeFunctionRegistry(FunctionContributions functionContributions) {
+        var functionRegistry = functionContributions.getFunctionRegistry();
+        var typeConfiguration = functionContributions.getTypeConfiguration();
+        functionRegistry.register("array_contains", new MongoArrayContainsFunction(typeConfiguration));
+    }
+
+    // https://www.mongodb.com/docs/manual/tutorial/query-arrays/
+    private static class MongoArrayContainsFunction extends AbstractArrayContainsFunction {
+
+        public MongoArrayContainsFunction(TypeConfiguration typeConfiguration) {
+            super(true, typeConfiguration);
+        }
+
+        @Override
+        public void render(
+                SqlAppender sqlAppender,
+                List<? extends SqlAstNode> sqlAstArguments,
+                ReturnableType<?> returnType,
+                SqlAstTranslator<?> walker) {
+            final Expression haystackExpression = (Expression) sqlAstArguments.get(0);
+            final Expression needleExpression = (Expression) sqlAstArguments.get(1);
+
+            final JdbcMappingContainer needleTypeContainer = needleExpression.getExpressionType();
+            final JdbcMapping needleType = needleTypeContainer == null ? null : needleTypeContainer.getSingleJdbcMapping();
+
+            if (needleType == null || needleType instanceof BasicPluralType<?, ?>) {
+                sqlAppender.append("{ ");
+                haystackExpression.accept(walker);
+                sqlAppender.append(": { $all: ");
+                needleExpression.accept(walker);
+                sqlAppender.append(" } }");
+            } else {
+                sqlAppender.append("{ ");
+                haystackExpression.accept(walker);
+                sqlAppender.append(": ");
+                needleExpression.accept(walker);
+                sqlAppender.append(" }");
+            }
+        }
+    }
 }
--- chameleon/src/test/java/org/hibernate/omm/mapping/EmbeddedArrayFieldTests.java
+++ chameleon/src/test/java/org/hibernate/omm/mapping/EmbeddedArrayFieldTests.java
@@ -8,6 +8,7 @@ import org.hibernate.omm.AbstractMongodbIntegrationTests;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -23,9 +24,7 @@ class EmbeddedArrayFieldTests extends AbstractMongodbIntegrationTests {
     @DisplayName("when collection field was persisted, it should be saved as embedded array in MongoDB")
     void test_persist_into_mongo_embedded_array_field() {
         var insertedMovie = getSessionFactory().fromTransaction(session -> {
-            var movie = new Movie();
-            movie.id = id;
-            movie.title = "Gone with Wind";
+            var movie = new Movie(id);
             movie.tags = List.of("romantic", "classic");
             session.persist(movie);
             return movie;
@@ -42,9 +41,7 @@ class EmbeddedArrayFieldTests extends AbstractMongodbIntegrationTests {
     @DisplayName("when collection field was persisted, it should be fetched as expected")
     void test_load_from_mongo_embedded_array_field() {
         var insertedMovie = getSessionFactory().fromTransaction(session -> {
-            var movie = new Movie();
-            movie.id = id;
-            movie.title = "Gone with Wind";
+            var movie = new Movie(id);
             movie.tags = List.of("romantic", "classic");
             session.persist(movie);
             return movie;
@@ -53,6 +50,41 @@ class EmbeddedArrayFieldTests extends AbstractMongodbIntegrationTests {
             var loadedMovie = new Movie();
             session.load(loadedMovie, id);
             assertThat(loadedMovie).usingRecursiveComparison().isEqualTo(insertedMovie);
+        });
+    }
+
+    @Test
+    @DisplayName("when collection field was queried by some entry, it should return result as expected")
+    void test_query_mongo_embedded_array_field() {
+        var romanticTag = "romantic";
+        var classicTag = "classic";
+
+        getSessionFactory().inTransaction(session -> {
+            var movieWithRomanticTagOnly = new Movie(1);
+            movieWithRomanticTagOnly.tags = List.of(romanticTag);
+            session.persist(movieWithRomanticTagOnly);
+
+            var movieWithClassicTagOnly = new Movie(2);
+            movieWithClassicTagOnly.tags = List.of(classicTag);
+            session.persist(movieWithClassicTagOnly);
+
+            var movieWithBothTags = new Movie(3);
+            movieWithBothTags.tags = List.of(romanticTag, classicTag);
+            session.persist(movieWithBothTags);
+
+            var movieWithNoTags = new Movie(4);
+            movieWithNoTags.tags = Collections.emptyList();
+            session.persist(movieWithNoTags);
+        });
+
+        getSessionFactory().inTransaction(session -> {
+            var queryStr = "from Movie m where array_contains(m.tags, :tag)";
+
+            var classicMovies = session.createQuery(queryStr, Movie.class).setParameter("tag", classicTag).list();
+            assertThat(classicMovies).extracting(Movie::getId).containsExactly(2, 3);
+
+            var romanticMovies = session.createQuery(queryStr, Movie.class).setParameter("tag", romanticTag).list();
+            assertThat(romanticMovies).extracting(Movie::getId).containsExactly(1, 3);
         });
     }
 
@@ -67,7 +99,15 @@ class EmbeddedArrayFieldTests extends AbstractMongodbIntegrationTests {
         @Id
         Integer id;
 
-        String title;
+        Movie() {}
+        Movie(Integer id) {
+            this.id = id;
+        }
+
+        Integer getId() {
+            return id;
+        }
+
         List<String> tags;
 
     }
