5800bb30487f984b5b0b9c2efdd2b572e4a536b2
nathan.xu <nathan.xu@procor.com>
2024-08-17 09:43:48 -0400

    add more stuff to MongoDatabaseMetaData to avoid awkward exception thrown during Hibernate's internal JDBC env bootstrapping process

--- chameleon-core/src/main/java/org/hibernate/omm/jdbc/MongoDatabaseMetaData.java
+++ chameleon-core/src/main/java/org/hibernate/omm/jdbc/MongoDatabaseMetaData.java
@@ -18,10 +18,19 @@ package org.hibernate.omm.jdbc;
 import org.hibernate.omm.jdbc.adapter.DatabaseMetaDataAdapter;
 
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 
 /**
+ * Virtual Mongo JDBC Driver's minimal metadata feature set to ensure no exception is thrown during JDBC Environment
+ * building.
+ * <p/>
+ * Needs to re-evaluate whether the provided metadata is correct. For the time being, they are
+ * provided as dummy overriding to avoid awkward exception during {@link org.hibernate.engine.jdbc.env.spi.JdbcEnvironment}
+ * building phase.
+ *
  * @author Nathan Xu
  * @since 1.0.0
+ * @see org.hibernate.engine.jdbc.env.internal.JdbcEnvironmentImpl
  */
 public class MongoDatabaseMetaData implements DatabaseMetaDataAdapter {
     private final String version;
@@ -67,4 +76,103 @@ public class MongoDatabaseMetaData implements DatabaseMetaDataAdapter {
         return "Virtual JDBC Driver";
     }
 
+    @Override
+    public int getDriverMajorVersion() {
+        return -1;
+    }
+
+    @Override
+    public int getDriverMinorVersion() {
+        return -1;
+    }
+
+    @Override
+    public String getSQLKeywords() {
+        return "";
+    }
+
+    @Override
+    public boolean supportsSchemasInTableDefinitions() {
+        return false;
+    }
+
+    @Override
+    public boolean supportsCatalogsInTableDefinitions() {
+        return false;
+    }
+
+    @Override
+    public boolean storesLowerCaseIdentifiers() {
+        return false;
+    }
+
+    @Override
+    public boolean storesUpperCaseIdentifiers() {
+        return false;
+    }
+
+    @Override
+    public boolean storesMixedCaseIdentifiers() {
+        return true;
+    }
+
+    @Override
+    public boolean storesUpperCaseQuotedIdentifiers() {
+        return false;
+    }
+
+    @Override
+    public boolean storesLowerCaseQuotedIdentifiers() {
+        return false;
+    }
+
+    @Override
+    public boolean storesMixedCaseQuotedIdentifiers() {
+        return true;
+    }
+
+    @Override
+    public boolean supportsNamedParameters() {
+        return true;
+    }
+
+    @Override
+    public boolean supportsResultSetType(int type) {
+        return false;
+    }
+
+    @Override
+    public boolean supportsGetGeneratedKeys() {
+        return true;
+    }
+
+    @Override
+    public boolean supportsBatchUpdates() {
+        return true;
+    }
+
+    @Override
+    public boolean dataDefinitionIgnoredInTransactions() {
+        return true;
+    }
+
+    @Override
+    public boolean dataDefinitionCausesTransactionCommit() {
+        return false;
+    }
+
+    @Override
+    public int getSQLStateType() {
+        return DatabaseMetaData.sqlStateSQL;
+    }
+
+    @Override
+    public String getCatalogSeparator() {
+        return "\n";
+    }
+
+    @Override
+    public boolean isCatalogAtStart() {
+        return true;
+    }
 }
