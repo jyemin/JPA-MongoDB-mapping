7fb64d664eaeda92a00d5e8c982bc4b3d5c95025
nathan.xu <nathan.xu@procor.com>
2024-08-22 15:44:47 -0400

    configure Mongo stores UUID in string format as opposed to default binary format

---
+++ chameleon-core/src/test/java/org/hibernate/omm/id/UuidSotrageTests.java
@@ -0,0 +1,79 @@
+/*
+ *
+ * Copyright 2008-present MongoDB, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package org.hibernate.omm.id;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import org.hibernate.SessionFactory;
+import org.hibernate.annotations.UuidGenerator;
+import org.hibernate.omm.extension.CommandRecorderInjected;
+import org.hibernate.omm.extension.MongoIntegrationTest;
+import org.hibernate.omm.extension.SessionFactoryInjected;
+import org.hibernate.omm.service.CommandRecorder;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Nathan Xu
+ * @since 1.0.0
+ */
+@MongoIntegrationTest
+class UuidSotrageTests {
+
+    @SessionFactoryInjected
+    SessionFactory sessionFactory;
+
+    @CommandRecorderInjected
+    CommandRecorder commandRecorder;
+
+    @Test
+    void test_string_representation() {
+        sessionFactory.inTransaction(session -> {
+            var book = new Book();
+            book.title = "The Last of the Mohicans";
+            book.author = "James Fenimore Cooper";
+            session.persist(book);
+        });
+
+        assertThat(commandRecorder.getCommandRecords()).hasSize(1)
+                .allSatisfy(command -> {
+                   assertThat(command.getArray("documents")).hasSize(1)
+                           .allSatisfy(doc -> {
+                               assertThat(doc.isDocument()).isTrue();
+                               assertThat(doc.asDocument().get("_id").isString())
+                                       .overridingErrorMessage("UUID value of String format instead of binary format should be stored")
+                                       .isTrue();
+                           });
+                });
+    }
+
+    @Entity(name = "Book")
+    static class Book {
+        @Id @GeneratedValue
+        @UuidGenerator
+        UUID id;
+
+        String title;
+        String author;
+    }
+}
