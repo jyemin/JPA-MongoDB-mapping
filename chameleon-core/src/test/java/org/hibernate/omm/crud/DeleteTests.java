9f238d555b3b8d04e06048dfa278aa912f0be1a4
Jeff Yemin <jeff.yemin@mongodb.com>
2024-08-31 18:41:14 -0400

    Add delete test with criteria

--- chameleon-core/src/test/java/org/hibernate/omm/crud/DeleteTests.java
+++ chameleon-core/src/test/java/org/hibernate/omm/crud/DeleteTests.java
@@ -15,7 +15,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  * @since 1.0.0
  */
 @MongoIntegrationTest
-class DeleteAllTests {
+class DeleteTests {
 
     @SessionFactoryInjected
     SessionFactory sessionFactory;
@@ -36,6 +36,28 @@ class DeleteAllTests {
         });
         sessionFactory.inTransaction(session -> {
             var query = session.createSelectionQuery("from Book", Book.class);
+            assertThat(query.getResultList()).isEmpty();
+        });
+    }
+
+    @Test
+    void test_delete_with_filter() {
+        sessionFactory.inTransaction(session -> {
+            var book1 = new Book(1L);
+            var book2 = new Book(2L);
+            var book3 = new Book(3L);
+            session.persist(book1);
+            session.persist(book2);
+            session.persist(book3);
+        });
+        sessionFactory.inTransaction(session -> {
+            var query = session.createMutationQuery("delete from Book where id = :id");
+            query.setParameter("id", 1L);
+            Assertions.assertDoesNotThrow(query::executeUpdate);
+        });
+        sessionFactory.inTransaction(session -> {
+            var query = session.createSelectionQuery("from Book where id = :id", Book.class);
+            query.setParameter("id", 1L);
             assertThat(query.getResultList()).isEmpty();
         });
     }
